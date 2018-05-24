57cb617ab2d2548db5cc3218b1dc9c23c6653423
sivanWu0222 <sivan0222@vip.qq.com>
2018-05-24 20:03:15 +0800

    add hello-world programme

---
+++ 2/2/1/hello.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+	/***
+	输出Hello, World的两种方法： 
+	*/ 
+	//方法一： 
+	//std::cout<< "Hello, World!\n";	
+	
+	//方法2：
+	using namespace std;
+	cout << "Hello, World";	
+	
+} 
