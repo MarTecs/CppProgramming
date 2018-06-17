3e704682ce26e2b29d8a2a2ef055e27a2700de34
sivanWu0222 <sivan0222@vip.qq.com>
2018-06-17 17:28:18 +0800

    add some examples

---
+++ c++ prime第5版/1/1.2/Test4.cpp
@@ -0,0 +1,12 @@
+
+/**
+ * 检测下列程序是否合法
+ * @return [description]
+ */
+#include<iostream>
+int main()
+{
+  std::cout << "The sum of " << v1;
+            << " and " << v2;
+            << " is " << v1 + v2 << std::endl;
+}
