1e50fc1f98560330227c1cb5b70a2876b7f9869d
sivanWu0222 <sivan0222@vip.qq.com>
2018-06-17 10:30:07 +0800

    init

--- 2/myfirst.cpp
+++ 2/myfirst.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
+
+
+// This is the content of this program
 int main()
 {
 	using namespace std;
 	std::cout << "Come up and C++ me some time";
 	cout << "You won't regret it!" << endl;
-	return 0;	
+	return 0;
 }
-
