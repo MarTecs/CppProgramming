193c80a71cf0a11f19ba927e42fc5d6a4561156d
sivanWu0222 <sivan0222@vip.qq.com>
2018-06-26 08:50:29 +0800

    add source files

--- c++ prime第5版/1/1.5/1_5_2_Test1.cpp
+++ c++ prime第5版/1/1.5/1_5_2_Test1.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include "Sales_item.h"
-
 /**
  * 编写程序，读取多条销售记录，并统计每个ISBN(每本书) 有几条销售记录
  * @return
  */
-
 int main()
 {
-
   Sales_item item1, item2, currItem;
   int cnt = 1;
-  while ( std::cin >> item1 )
+  if ( std::cin >> item1 )
   {
     currItem = item1;
-    if ( (std::cin >> item2) && (currItem.isbn() == item2.isbn()) )
+    while ( std::cin >> item2 )
     {
-      ++cnt;
-    }else {
-      std::cout << currItem << " occurs " << cnt << " times" << std::endl;
+      if ( currItem.isbn() == item2.isbn() )
+      {
+        currItem += item2;
+        cnt++;
+      } else {
+        std::cout << currItem << " occurs " << cnt << " times" << std::endl;
+        currItem = item2;
+        cnt = 1;
+      }
     }
+    std::cout << item1 << " occurs " << cnt << " times" << std::endl;
   }
-
   return 0;
 }
