ab594731cd9af4b3559e67b65ebdd473677df710
sivanWu0222 <sivan0222@vip.qq.com>
2018-06-30 08:56:50 +0800

    add

---
+++ c++ prime第5版/3/3.4/3.4.1/Test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+  string s("Hello World");
+  cout << s.begin() << endl;
+  cout << s.begin() << endl;
+
+
+
+  return 0;
+}
